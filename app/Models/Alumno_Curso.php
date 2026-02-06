5b188dbfe502c622a21524e0a6d5707d213e775e
PIkOOESP <miguelangel.s.b86@gmail.com>
2026-02-06 17:12:37 +0100

    Clases sin completar

---
+++ app/Models/Alumno_Curso.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Alumno_Curso extends Model
+{
+    protected $fillable = ["alumno_id", "curso_id"];
+    protected $hidden = ["created_at","updated_at"];
+
+    function alumno(){
+        return $this -> BelongsTo(Alumno::class);
+    }
+
+    function curso(){
+        return $this -> belongsTo(Curso::class);
+    }
+}
---
+++ database/migrations/2026_02_06_155408_create_clase__alumno__cursos_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clase__alumno__cursos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clase__alumno__cursos');
+    }
+};
