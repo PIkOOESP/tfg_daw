48b1374be28ff69008c37b4d9d69812c2d34cc99
PIkOOESP <miguelangel.s.b86@gmail.com>
2026-02-12 13:31:45 +0100

    Id arreglado

--- resources/views/clase.blade.php
+++ resources/views/clase.blade.php
@@ -69,7 +69,7 @@
                                     <div class="card-body">
                                         <h5 class="card-title">{{ $asignacion->nombre_alumno }} {{ $asignacion->apellido_alumno }}</h5>
                                     </div>
-                                    <form action="{{ route('claseAlumno.miniBorrar', ['clase_alumno_curso_id' => $asignacion->id, 'curso_id'  => $value['curso_id'], 'clase_id'  => $value['clase_id']]) }}" method="POST" class="mt-2">
+                                    <form action="{{ route('claseAlumno.miniBorrar', ['clase_alumno_curso_id' => $asignacion->cac_id, 'curso_id'  => $value['curso_id'], 'clase_id'  => $value['clase_id']]) }}" method="POST" class="mt-2">
                                         @csrf
                                         @method('delete')
                                         <button type="submit" class="btn btn-outline-warning btn-sm">
